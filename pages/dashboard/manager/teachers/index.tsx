9d0f15d7a4a67cbfa008ec49446d5387c8625c71
yume <yumeee.lol@gmail.com>
2022-03-13 18:31:35 +1100

    menu updata

--- pages/dashboard/manager/teachers/index.tsx
+++ pages/dashboard/manager/teachers/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "antd/dist/antd.css";
-import ManagerLayout from "../../../../components/student/manager-layout";
+import ManagerLayout from "../../../../components/layout";
 
 
 
