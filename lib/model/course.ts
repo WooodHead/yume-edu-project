ee8ae2e9aa21e415bdb02595d5558d6040aec58c
yume <yumeee.lol@gmail.com>
2022-03-20 06:07:37 +1100

    course schedule update

--- lib/model/course.ts
+++ lib/model/course.ts
@@ -7,35 +7,35 @@ export interface CourseShort {
 }
 
 export interface ICourseDetails {
-    children?: ReactNode;
-    cover: string;
-    name: string;
-    id: number;
-    duration: number;
-    teacherName: string;
-    teacherId: number;
-    maxStudents: number;
-    startTime: string;
-    star: number;
-    createdAt: string;
-    uid: string;
-    detail: string;
-    sales: ICourseSales;
-    schedule: ISchedule;
-    type: IType;
-  }
+  children?: ReactNode;
+  cover: string;
+  name: string;
+  id: number;
+  duration: number;
+  teacherName: string;
+  teacherId: number;
+  maxStudents: number;
+  startTime: string;
+  star: number;
+  createdAt: string;
+  uid: string;
+  detail: string;
+  sales: ICourseSales;
+  schedule: ISchedule;
+  type: IType;
+}
 
-  export interface CourseCardProps {
-    id: number,
-    cover:string,
-    name: string,
-    duration: number,
-    teacherName: string,
-    maxStudents: number,
-    startTime: string,
-    star: number,
-    children?: ReactNode,
-  }
+export interface CourseCardProps {
+  id: number;
+  cover: string;
+  name: string;
+  duration: number;
+  teacherName: string;
+  maxStudents: number;
+  startTime: string;
+  star: number;
+  children?: ReactNode;
+}
 
 export interface ICourseSales {
   price: number;
@@ -49,16 +49,37 @@ export interface IType {
 }
 
 export interface ISchedule {
-  classTime: [string];
-  chapters: IChapters;
+  classTime: string[];
+  chapters: {name: string, content: string}[];
+}
+
+export interface IScheduleReq extends ISchedule  {
+  scheduleId: number | null;
+  courseId?: number | null;
 }
 
 export interface IChapters {
-  map: any;
+  name: string;
   content: string;
   createdAt: string;
   id: number;
-  name: string;
   order: number;
   updatedAt: string;
 }
+
+export interface ICourse {
+  cover?: string;
+  detail: string;
+  duration: number;
+  durationUnit: number;
+  maxStudents: number;
+  name: string;
+  price: number;
+  startTime?: string;
+  teacherId: number;
+  uid: string;
+  type: {};
+}
+
+
+
